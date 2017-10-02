069f3ff4fbd2b2267ee7fe795464ddfe7776c0b4
Munemori <munemori.takemura@gmail.com>
2017-10-02 10:55:31 +0200

    Add project files.

---
+++ PasswordMaker/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace PasswordMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.chk_low = new System.Windows.Forms.CheckBox();
+            this.chk_num = new System.Windows.Forms.CheckBox();
+            this.chk_hi = new System.Windows.Forms.CheckBox();
+            this.chk_symb = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Karakterek száma (8-18)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(153, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            18,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // chk_low
+            // 
+            this.chk_low.AutoSize = true;
+            this.chk_low.Location = new System.Drawing.Point(28, 49);
+            this.chk_low.Name = "chk_low";
+            this.chk_low.Size = new System.Drawing.Size(67, 17);
+            this.chk_low.TabIndex = 4;
+            this.chk_low.Text = "Kisbetűk";
+            this.chk_low.UseVisualStyleBackColor = true;
+            this.chk_low.CheckedChanged += new System.EventHandler(this.chk_low_CheckedChanged);
+            // 
+            // chk_num
+            // 
+            this.chk_num.AutoSize = true;
+            this.chk_num.Location = new System.Drawing.Point(28, 95);
+            this.chk_num.Name = "chk_num";
+            this.chk_num.Size = new System.Drawing.Size(64, 17);
+            this.chk_num.TabIndex = 5;
+            this.chk_num.Text = "Számok";
+            this.chk_num.UseVisualStyleBackColor = true;
+            this.chk_num.CheckedChanged += new System.EventHandler(this.chk_num_CheckedChanged);
+            // 
+            // chk_hi
+            // 
+            this.chk_hi.AutoSize = true;
+            this.chk_hi.Location = new System.Drawing.Point(28, 72);
+            this.chk_hi.Name = "chk_hi";
+            this.chk_hi.Size = new System.Drawing.Size(78, 17);
+            this.chk_hi.TabIndex = 6;
+            this.chk_hi.Text = "Nagybetűk";
+            this.chk_hi.UseVisualStyleBackColor = true;
+            this.chk_hi.CheckedChanged += new System.EventHandler(this.chk_hi_CheckedChanged);
+            // 
+            // chk_symb
+            // 
+            this.chk_symb.AutoSize = true;
+            this.chk_symb.Location = new System.Drawing.Point(28, 118);
+            this.chk_symb.Name = "chk_symb";
+            this.chk_symb.Size = new System.Drawing.Size(88, 17);
+            this.chk_symb.TabIndex = 7;
+            this.chk_symb.Text = "Szimbólumok";
+            this.chk_symb.UseVisualStyleBackColor = true;
+            this.chk_symb.CheckedChanged += new System.EventHandler(this.chk_symb_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 86);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Létrehoz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(28, 141);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 134);
+            this.listBox1.TabIndex = 9;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(39, 280);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(164, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://www.passwordmeter.com/";
+            this.linkLabel1.Click += new System.EventHandler(this.linkLabel1_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(31, 300);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(183, 13);
+            this.linkLabel2.TabIndex = 11;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://howsecureismypassword.net/";
+            this.linkLabel2.Click += new System.EventHandler(this.linkLabel2_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(0, 322);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 19);
+            this.label2.TabIndex = 12;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(241, 341);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chk_symb);
+            this.Controls.Add(this.chk_hi);
+            this.Controls.Add(this.chk_num);
+            this.Controls.Add(this.chk_low);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(257, 380);
+            this.MinimumSize = new System.Drawing.Size(257, 380);
+            this.Name = "Form1";
+            this.Text = "Password Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox chk_low;
+        private System.Windows.Forms.CheckBox chk_num;
+        private System.Windows.Forms.CheckBox chk_hi;
+        private System.Windows.Forms.CheckBox chk_symb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PasswordMaker/Form1.cs
@@ -0,0 +1,484 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace PasswordMaker
+{
+    public partial class Form1 : Form
+    {
+        byte type = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // The characters to encode:
+        //Kisbetűk:97-122
+        //Nagybetűk: 65-90
+        //Számok: 48-57
+        //Szimbólumok: 33-47
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (chk_low.Checked == false & chk_num.Checked == false & chk_hi.Checked == false & chk_symb.Checked == false)
+            {
+                MessageBox.Show("Legalább egyet ki kell választani a négy lehetőség közül", "Beállítási hiba", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+                
+            }
+            if (chk_low.Checked == true & chk_num.Checked == false & chk_hi.Checked == false & chk_symb.Checked == false)
+            {
+                //MessageBox.Show("Csak kis betűk");
+                //OnlyLow();
+                listBox1.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    OnlyOneType(97, 123,type); //Kisbetűk:97-122
+                }
+                return;
+            }
+            if (chk_low.Checked == false & chk_num.Checked == true & chk_hi.Checked == false & chk_symb.Checked == false)
+            {
+                //MessageBox.Show("Csak számok");
+                listBox1.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    //Késleltetés();
+                    //OnlyNumbers();
+                    OnlyOneType(48, 58,type); //számok: 48-57
+                }
+                return;
+            }
+            if (chk_low.Checked == false & chk_num.Checked == false & chk_hi.Checked == true & chk_symb.Checked == false)
+            {
+                //MessageBox.Show("Csak nagy betűk");
+                //OnlyHi();
+                listBox1.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    OnlyOneType(65, 91,type);//Nagybetűk: 65-90
+                }
+                return;
+            }
+            if (chk_low.Checked == false & chk_num.Checked == false & chk_hi.Checked == false & chk_symb.Checked == true)
+            {
+                //MessageBox.Show("Csak szimbólumok");
+                listBox1.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    OnlyOneType(33, 48,type);//Szimbólumok: 33-47
+                }
+                return;
+            }
+            else
+            {
+                listBox1.Items.Clear();
+                for (int i = 0; i < 10; i++)
+                {
+                    OnlyOneType(0, 0, type);
+                }
+            }
+        }
+
+        //nagybetű - kisbetű - szimbolum - kisbetű - nagybetű - szám - nagybetű - szimbolum - kisbetű - szimbolum - szám
+        //kisbetű - szimbolum - nagybetű - szám - nagybetű
+
+        private void OnlyLow()
+        {
+            MessageBox.Show("OnlyLow");
+        }
+        private void OnlyHi()
+        {
+            MessageBox.Show("OnlyHi");
+        }
+        private void OnlyNumbers()
+        {
+            //MessageBox.Show("Only Numbers");
+            //Random rnd = new Random();
+            //int KarakterSzám = Convert.ToInt32(numericUpDown1.Value);
+            //int[] pass = new int[KarakterSzám];
+            //while (KarakterSzám>0)
+            //{
+            //    pass[KarakterSzám-1] = Convert.ToInt32(rnd.Next(0, 9));
+            //    KarakterSzám--;
+            //}
+
+            //MessageBox.Show("Only Numbers");
+
+            Késleltetés();
+            int KarakterSzám = Convert.ToInt32(numericUpDown1.Value);
+            string Pass = "";
+                Random rnd = new Random();
+                while (KarakterSzám > 0)
+                {
+                rnd.Next();
+                    Pass = Pass + rnd.Next(0, 10).ToString();
+                    KarakterSzám--;
+                }
+
+                listBox1.Items.Add(Pass);
+                listBox1.Update();
+            
+        }
+        private void OnlySymb()
+        {
+            MessageBox.Show("OnlySymb");
+
+        }
+        private void Késleltetés()
+        {
+            System.Threading.Thread.Sleep(15);
+            //for (double j = 0; j < 100901; j++)
+            //{
+            //    listBox1.Update();
+            //}
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string CopyPass = listBox1.SelectedItem.ToString();
+            if (CopyPass != "")
+            {
+                Clipboard.SetText(CopyPass);
+            }
+
+            label2.Text = CopyPass;
+            
+        }
+        private void OnlyOneType(int Min, int Max,byte Type)//típus átadás esetén switch/case
+        {
+            Késleltetés();
+            int KarakterSzám = Convert.ToInt32(numericUpDown1.Value);
+            string Pass = "";
+            Random rnd = new Random();
+            switch (Type)
+            {
+                case 1:
+                    //Debug.Print("kb");
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(Min, Max)).ToString();//97-122
+                        --KarakterSzám;
+                    }
+                    break;
+                case 2:
+                    //Debug.Print("nb");
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(Min, Max)).ToString();//65-90
+                        --KarakterSzám;
+                    }
+                    break;
+                case 3:
+                    //Debug.Print("kb-nb");
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();//97-122 és 65-90
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(65, 91)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 4:
+                    //Debug.Print("szá");
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(Min, Max)).ToString();//48-57
+                        --KarakterSzám;
+                    }
+                    break;
+                case 5:
+                    //Debug.Print("kb-szá");//97-122  48-57
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 6:
+                    //Debug.Print("nb-szá");//65-90   48-57
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(65, 91)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 7:
+                    //Debug.Print("kb-nb-szá");//97-122   65-90   48-57
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        if (rnd.Next(1,3)==2)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(65, 91)).ToString();
+                        }
+                        else
+                        {
+                            Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        }
+                        --KarakterSzám;
+                        //if (KarakterSzám == 0) break;
+                        //Debug.Print(KarakterSzám.ToString());
+                        //Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        //--KarakterSzám;
+                    }
+                    break;
+                case 8:
+                    //Debug.Print("szi");
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(Min, Max)).ToString();//33-47
+                        --KarakterSzám;
+                    }
+                    break;
+                case 9:
+                    //Debug.Print("kb-szi");//97-122  33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 10:
+                    //Debug.Print("nb-szi");//65-90   33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(65, 91)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 11:
+                    //Debug.Print("kb-nb-szi");//97-122   65-90   33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        if (rnd.Next(1, 3) == 2)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(65, 91)).ToString();
+                        }
+                        else
+                        {
+                            Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        }
+                        --KarakterSzám;
+                        
+                    }
+                    break;
+                case 12:
+                    //Debug.Print("szá-szi");//48-57  33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        Debug.Print(KarakterSzám.ToString());
+                        Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        --KarakterSzám;
+                    }
+                    break;
+                case 13:
+                    //Debug.Print("kb-szá-szi");//97-122  48-57   33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(97, 123)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        if (rnd.Next(1, 3) == 2)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        }
+                        else
+                        {
+                            Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        }
+                        --KarakterSzám;
+
+                    }
+                    break;
+                case 14:
+                    //Debug.Print("nb-szá-szi");//65-90   48-57   33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(65, 90)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        if (rnd.Next(1, 3) == 2)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(48, 58)).ToString();
+                        }
+                        else
+                        {
+                            Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        }
+                        --KarakterSzám;
+
+                    }
+                    break;
+                case 15:
+                    //Debug.Print("mind");//97-122    65-90   48-57   33-47
+                    while (KarakterSzám > 0)
+                    {
+                        rnd.Next();
+                        Pass += Convert.ToChar(rnd.Next(65, 90)).ToString();
+                        --KarakterSzám;
+                        if (KarakterSzám == 0) break;
+                        int typC = rnd.Next(2, 5);
+                        if (typC == 2)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(97,123)).ToString();
+                        }
+                        else if(typC==3)
+                        {
+                            Pass += Convert.ToChar(rnd.Next(48,58)).ToString();
+                        }
+                        else
+                        {
+                            Pass += Convert.ToChar(rnd.Next(33, 48)).ToString();
+                        }
+                        --KarakterSzám;
+
+                    }
+                    break;
+                default:
+                    break;
+            }
+            //int KarakterSzám = Convert.ToInt32(numericUpDown1.Value);
+            //char PassCh ='\0';
+            //string Pass = "";
+            //Random rnd = new Random();
+            //while (KarakterSzám > 0)
+            //{
+            //    rnd.Next();
+            //    //PassCh = Convert.ToChar(rnd.Next(Min, Max));
+            //    Pass += Convert.ToChar(rnd.Next(Min, Max)).ToString();
+            //    KarakterSzám--;
+            //}
+
+            listBox1.Items.Add(Pass);
+            listBox1.SelectedIndex = 0;
+            listBox1.Update();
+        }
+
+
+
+        private void chk_low_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_low.Checked==true)
+            {
+                type++;
+                Debug.Print(type.ToString());
+            }
+            else
+            {
+                type--;
+                Debug.Print(type.ToString());
+            }
+            
+        }
+
+        private void chk_hi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_hi.Checked==true)
+            {
+                type += 2;
+                Debug.Print(type.ToString());
+            }
+            else
+            {
+                type -= 2;
+                Debug.Print(type.ToString());
+            }
+        }
+
+        private void chk_num_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_num.Checked == true)
+            {
+                type += 4;
+                Debug.Print(type.ToString());
+            }
+            else
+            {
+                type -= 4;
+                Debug.Print(type.ToString());
+            }
+        }
+
+        private void chk_symb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chk_symb.Checked == true)
+            {
+                type += 8;
+                Debug.Print(type.ToString());
+            }
+            else
+            {
+                type -= 8;
+                Debug.Print(type.ToString());
+            }
+        }
+
+        private void linkLabel1_Click(object sender, EventArgs e)
+        {
+            Process.Start(linkLabel1.Text);
+        }
+
+        private void linkLabel2_Click(object sender, EventArgs e)
+        {
+            Process.Start(linkLabel2.Text);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
